915bbbd191d8c719d637f5e22e8d719fb1440dda
frogteam <frogteam@naver.com>
2024-09-20 17:14:44 +0900

    240920

---
+++ Lec10_Loop/src/practice/gugu2/Gugu2.java
@@ -0,0 +1,44 @@
+package practice.gugu2;
+
+import java.util.Scanner;
+
+public class Gugu2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // TODO
+
+        sc.close();
+    } // end main()
+} // end class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
